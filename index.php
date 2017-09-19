365f8fbecf437c3a790f6dfe351fcf322faee772
Manuel Thalmann <m@nuth.ch>
2017-09-19 23:22:56 +0200

    Completely reworked the menubar-schema

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-    array_key_exists('File', $_GET) AND file_exists($_GET['File']) OR DIE('Restricted Access');
+    array_key_exists('File', $_GET) AND file_exists($_GET['File']) OR DIE();
     echo file_get_contents($_GET['File']);
 ?>
